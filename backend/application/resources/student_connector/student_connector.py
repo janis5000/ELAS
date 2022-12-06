473b38765ab7a01f9aefbb6b1f0e6e8cb94648df
janis5000 <jaarsel1@gmail.com>
2022-12-06 17:52:15 +0100

    Started implementing routes

--- backend/application/resources/student_connector/student_connector.py
+++ backend/application/resources/student_connector/student_connector.py
@@ -1,9 +1,19 @@
 from flask import Blueprint
+from orm_interface.base import Base, Session, engine
+from orm_interface.entities.studyprogram import StudyProgram
 
 student_connector = Blueprint("student_connector", __name__)
 
+session = Session()
 
 @student_connector.route("/home")
 @student_connector.route("/")
-def student_connector_home():
+def home():
     return "Student Connector Home"
+
+@student_connector.route("/study-programs", methods=["GET"])
+def get_all_study_programs():
+    all_studyprograms = session.query(StudyProgram).all()
+
+@student_connector.route("/profile/<id>", methods=["GET"])
+def get_profile(id):
