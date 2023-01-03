1fc9ed458510e3112db3eadb6dfaaccddbe98b2c
janis5000 <jaarsel1@gmail.com>
2023-01-03 20:27:53 +0100

    Changed student_connector_user model and extended route for setting the profile

--- backend/application/resources/student_connector/student_connector.py
+++ backend/application/resources/student_connector/student_connector.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify, request, abort
 from orm_interface.base import Base, Session, engine
-from orm_interface.entities.student_connector_entity.student_connector_user import Student_Connector_User
+from orm_interface.entities.student_connector_entity.student_connector_user import Student_Connector_User, \
+    Student_Connector_Skills
 from orm_interface.entities.studyprogram import StudyProgram
 
 student_connector = Blueprint("student_connector", __name__)
@@ -30,13 +31,23 @@ def set_profile_description(id):
     # this is only a dummy authentication, it is completely useless!
     profile_attributes = request.form
     if id == profile_attributes['check']:
-        user = session.query(Student_Connector_User).filter(Student_Connector_User.id == id)
+        user_db = session.query(Student_Connector_User).filter(Student_Connector_User.id == id)
+        user = user_db.first()
         if 'description' in profile_attributes:
-            user.update({'description': profile_attributes['description']})
+            user_db.update({'description': profile_attributes['description']})
         if 'skills' in profile_attributes:
-            user.skills.extend(profile_attributes['skills'])
+            skill = session.query(Student_Connector_Skills).filter(Student_Connector_Skills.skill_name == profile_attributes['skills']).first()
+            if skill is not None:
+                user.skills.append(skill)
+            else:
+                skills = []
+                for skill in profile_attributes.getlist('skills'):
+                    skill_db = Student_Connector_Skills(skill)
+                    skills.append(skill_db)
+                    session.add(skill_db)
+                user.skills.extend(skills)
         if 'degree_id' in profile_attributes:
-            user.update({'degree_id': profile_attributes['degree_id']})
+            user_db.update({'degree_id': profile_attributes['degree_id']})
         session.commit()
         return jsonify("Successfully changed!")
     abort(403)
--- backend/orm_interface/entities/student_connector_entity/student_connector_user.py
+++ backend/orm_interface/entities/student_connector_entity/student_connector_user.py
@@ -1,7 +1,13 @@
-from sqlalchemy import Boolean, Column, Integer, String, Float, ForeignKey
+from sqlalchemy import Boolean, Column, Integer, String, Float, ForeignKey, Table
 from orm_interface.base import Base
 from sqlalchemy.orm import relationship
 
+Student_Connector_Skills_User = Table(
+    "student_connector_skills_user",
+    Base.metadata,
+    Column('skill_id', Integer, ForeignKey('student_connector_skills.id'), primary_key=True),
+    Column('user_id', Integer, ForeignKey('student_connector_user.id'), primary_key=True)
+)
 
 class Student_Connector_User(Base):
     __tablename__ = 'student_connector_user'
@@ -11,7 +17,7 @@ class Student_Connector_User(Base):
     degree_id = Column(String, ForeignKey('study_program.id'))
     sc_degree = relationship('StudyProgram', back_populates="sc_user")
     skills = relationship('Student_Connector_Skills',
-        secondary='student_connector_skills_user', back_populates="user"
+        secondary=Student_Connector_Skills_User, back_populates="user"
     )
 
     def __init__(self, id, email, description, languages, degree_id):
@@ -24,16 +30,15 @@ class Student_Connector_User(Base):
 class Student_Connector_Skills(Base):
     __tablename__ = 'student_connector_skills'
     id = Column(Integer, primary_key=True, autoincrement=True)
-    skill_name = Column(String)
+    skill_name = Column(String, unique=True)
     user = relationship('Student_Connector_User',
         secondary='student_connector_skills_user', back_populates="skills"
     )
 
-    def __init__(self, id, skill_name):
-        self.id = id
+    def __init__(self, skill_name):
         self.skill_name = skill_name
 
-class Student_Connector_Skills_User(Base):
+"""class Student_Connector_Skills_User(Base):
     __tablename__ = 'student_connector_skills_user'
     skill_id = Column(Integer, ForeignKey('student_connector_skills.id'), primary_key=True)
-    user_id = Column(Integer, ForeignKey('student_connector_user.id'), primary_key=True)
+    user_id = Column(Integer, ForeignKey('student_connector_user.id'), primary_key=True)"""
