f1a32f65570982bbb7f847fe626258434da21dca
janis5000 <jaarsel1@gmail.com>
2023-01-05 23:36:25 +0100

    WIP: Chat room database table

--- backend/orm_interface/entities/student_connector_entity/student_connector_user.py
+++ backend/orm_interface/entities/student_connector_entity/student_connector_user.py
@@ -16,6 +16,26 @@ Student_Connector_Courses_User = Table(
     Column('user_id', Integer, ForeignKey('student_connector_user.id'), primary_key=True)
 )
 
+Student_Connector_Chat_Room_User = Table(
+    "student_connector_chat_room_user",
+    Base.metadata,
+    Column('chat_id', Integer, ForeignKey('student_connector_chat_room.id'), primary_key=True),
+    Column('user_id', Integer, ForeignKey('student_connector_user.id'), primary_key=True)
+)
+
+class Student_Connector_Chat_Room(Base):
+    __tablename__ = 'student_connector_chat_room'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    user = relationship('Student_Connector_User', secondary='student_connector_chat_room_user', back_populates='chats')
+    messages = relationship('Student_Connector_Messages', back_populates='chat')
+
+class Student_Connector_Messages(Base):
+    __tablename__ = 'student_connector_messages'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    chat_id = Column(Integer, ForeignKey('student_connector_chat_room.id'))
+    chat = relationship('Student_Connector_Chat_Room', back_populates='messages')
+    message = Column(String)
+
 class Student_Connector_User(Base):
     __tablename__ = 'student_connector_user'
     id = Column(Integer, unique=True, nullable=False, autoincrement=True)
@@ -23,6 +43,7 @@ class Student_Connector_User(Base):
     description = Column(String)
     degree_id = Column(String, ForeignKey('study_program.id'))
     courses = relationship('Lecture', secondary='student_connector_courses_user', back_populates='sc_user')
+    chats = relationship('Student_Connector_Chat_Room', secondary='student_connector_chat_room_user', back_populates='user')
     sc_degree = relationship('StudyProgram', back_populates="sc_user")
     skills = relationship('Student_Connector_Skills',
         secondary='student_connector_skills_user', back_populates="user"
